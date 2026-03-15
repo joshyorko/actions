1d4892a51439f38ef5b462a970da4ecab27727b2
Josh Yorko <54248591+joshyorko@users.noreply.github.com>
2026-03-15 13:04:39 -0400

    fix: upgrade community RCC to v18.17.3 (#66)

--- action_server/build.py
+++ action_server/build.py
@@ -10,7 +10,7 @@ log = logging.getLogger(__name__)
 
 # Note: referenced here and in sema4ai.action_server._download_rcc
 # Using joshyorko/rcc open-source version
-RCC_VERSION = "18.17.1"
+RCC_VERSION = "18.17.3"
 
 
 CURDIR = Path(__file__).parent.absolute()
